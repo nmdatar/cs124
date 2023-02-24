80a875a13d8117f0a81ed3e216a2f6ba799a61e7
Matt Tengtrakool <matt_tengtrakool@college.harvard.edu>
2023-02-24 17:43:03 -0500

    changed all four

--- progset1/kruskal.cc
+++ progset1/kruskal.cc
@@ -5,6 +5,7 @@
 #include <unordered_map>
 #include <algorithm>
 
+
 using namespace std;
 
 struct four_coordinate {
@@ -135,7 +136,7 @@ vector<std::unordered_map<double, double> > generateCompleteGraph(int n) {
     int count = 0;
     for (int u = 0; u < n; u++) {
         for (int v = u + 1; v < n; v++) {
-            double weight = sqrt(pow(locs[u].a - locs[v].a, 2) + pow(locs[u].b - locs[v].b, 2) + pow(locs[u].c - locs[v].c, 2) + pow(locs[u].d - locs[v].d, 2));
+            double weight = pow(locs[u].a - locs[v].a, 2) + pow(locs[u].b - locs[v].b, 2) + pow(locs[u].c - locs[v].c, 2) + pow(locs[u].d - locs[v].d, 2);
             // if (weight < .18 * pow(2, (-1 * n / 128))) {
                 graph[u].insert(make_pair(v, weight));
                 count++;
@@ -145,6 +146,7 @@ vector<std::unordered_map<double, double> > generateCompleteGraph(int n) {
     }*/
 
     //UNCOMMENT FOR 3-D COORDINATE EXAMPLE
+    
      vector<three_coordinate > locs;
      for (int i = 0; i < n; i++) {
          three_coordinate loc = {.x = (double) rand() / RAND_MAX, .y = (double) rand() / RAND_MAX, .z = (double) rand() / RAND_MAX};
@@ -171,17 +173,11 @@ vector<std::unordered_map<double, double> > generateCompleteGraph(int n) {
     // int count = 0;
     // for (int u = 0; u < n; u++) {
     //     for (int v = u + 1; v < n; v++) {
-    //         double weight = sqrt(pow(locs[u].first - locs[v].first, 2) + pow(locs[u].second - locs[v].second, 2));
-    //         if (n <= 32768 && weight < .18) {
+    //         double weight = pow(locs[u].first - locs[v].first, 2) + pow(locs[u].second - locs[v].second, 2);
+    //         if (weight < 1.74*pow(n,-0.489)) {
     //             graph[u].insert(make_pair(v, weight));
     //             count++;
     //         }
-
-    //         else if (weight < .008){
-    //             graph[u].insert(make_pair(v, weight));
-    //             count++;
-    //         }
-            
     //     }
     // }
 
